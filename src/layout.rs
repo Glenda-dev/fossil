33d1362b1d59d06fb917b174a0d50b6f18e5275c
2018wzh <wzh20070720@outlook.com>
2026-02-25 16:47:27 +0800

    [kernel] Sync

--- src/layout.rs
+++ src/layout.rs
@@ -3,7 +3,7 @@ pub const RING_ENTRIES: u32 = 4;
 
 use glenda::cap::{CapPtr, Endpoint};
 
-pub const INIT_SLOT: CapPtr = CapPtr::from(10);
-pub const DEVICE_SLOT: CapPtr = CapPtr::from(11);
+pub const INIT_SLOT: CapPtr = CapPtr::from(14);
+pub const DEVICE_SLOT: CapPtr = CapPtr::from(15);
 pub const INIT_CAP: Endpoint = Endpoint::from(INIT_SLOT);
 pub const DEVICE_CAP: Endpoint = Endpoint::from(DEVICE_SLOT);
